927e09e1f47e94c4e9c7d059aef0479b3cd09596
bshongwe <dru.da.beat.hustle@gmail.com>
2026-06-30 00:45:25 +0200

    fix: URL support re-config

---
+++ backend/prisma.config.ts
@@ -0,0 +1,11 @@
+/**
+ * The datasource property `url` is no longer supported in schema files.
+ * Check Prisma's documentation for more information: https://pris.ly/d/datasource-url.
+ * Hence, removed it from schema.prisma
+ */
+import { defineConfig } from 'prisma/config';
+import 'dotenv/config';
+
+export default defineConfig({
+  datasourceUrl: process.env.DATABASE_URL,
+});
