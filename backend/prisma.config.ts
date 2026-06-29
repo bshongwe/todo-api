0b593eaeff64e795484beb689cf7cc80b05db073
bshongwe <dru.da.beat.hustle@gmail.com>
2026-06-30 01:09:26 +0200

    fix: URL support update

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -7,5 +7,16 @@ import { defineConfig } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-  datasourceUrl: process.env.DATABASE_URL,
+  earlyAccess: true,
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+  migrate: {
+    async adapter() {
+      const { PrismaPg } = await import('@prisma/adapter-pg');
+      const connectionString = process.env.DATABASE_URL!;
+      return new PrismaPg({ connectionString });
+    },
+  },
 });
