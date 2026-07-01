141ba1ce3ec9d8b8b9f51fad3fc30f93d152bd5a
bshongwe <dru.da.beat.hustle@gmail.com>
2026-07-01 14:13:30 +0200

    feat: Create method init

---
+++ backend/src/controllers/todoController.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { todoService } from '../services/todoService';
+import { asyncHandler } from '../utils/errorHandler';
+import { todoSchema } from '../middleware/validate';
+
+export const todoController = {
+  create: asyncHandler(async (req: Request, res: Response) => {
+    const validated = todoSchema.parse(req.body);
+    const todo = await todoService.createTodo(validated, req.user!.id); // <<assume>> auth middleware
+    res.status(201).json(todo);
+  }),
+
+  getAll: asyncHandler(async (req: Request, res: Response) => {
+    const todos = await todoService.getTodos(req.user!.id, req.query);
+    res.json(todos);
+  }),
+
+};
