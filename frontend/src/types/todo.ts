fb1b392a382bc8368fb755e12ddd461b4f3ad2d5
bshongwe <dru.da.beat.hustle@gmail.com>
2026-07-04 16:50:12 +0200

    feat: Added interface

--- frontend/src/types/todo.ts
+++ frontend/src/types/todo.ts
@@ -40,3 +40,20 @@ export interface ApiError {
     message: string;
   }>;
 }
+
+export interface User {
+  id: string;
+  email: string;
+  name?: string | null;
+}
+
+export interface RegisterDto {
+  email: string;
+  password: string;
+  name?: string;
+}
+
+export interface LoginDto {
+  email: string;
+  password: string;
+}
